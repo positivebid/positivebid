bb15f93574a659cb94fe26fca6141a9ebc03432b
Jason Lee <jlsync@gmail.com>
2013-01-11 16:32:58 +0000

    token update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Positive::Application.config.session_store :cookie_store, key: '_positive_session'
+Positive::Application.config.session_store :cookie_store, key: '_positive_sessionX'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
